5ffb9ace26a5eb4ae5f84147732d15d2e272490b
Kayla <fasreaperearnest@gmail.com>
2022-05-23 22:15:47 -0700

    keep the window open and do some processing the return of the agressively magenta window

--- src/app.cxx
+++ src/app.cxx
@@ -41,7 +41,7 @@ void Application::initWindow() {
 	#endif
 
 	//create window
-	window = glfwCreateWindow(WIDTH, HEIGHT, "graphic moment, really crazy stuff, though i winder if theres a limit to how long i can make this wondow title", NULL, NULL);
+	window = glfwCreateWindow(WIDTH, HEIGHT, "graphic moment, really crazy stuff, though i winder if theres a limit to how long i can make this wondow title as there is nothing telling me i have any kind of limit to how long this thing can be, so ill just keep typing this out until somthing happens, though so far nothing has happened i suppose so i will just continue on typing until this probably creates a overflow error though its noy my fault the GLFW devs dident think that someone might just make their title this long honestly, this reminds me of this one time, what? did you think i was gonna tell you what  this one time was? thats an invasion of my privacy, all i said was that i was reminded of somthing", NULL, NULL);
 
 	if (window == NULL) { // if the window couldent be made then its ok to error out
 		glfwTerminate();
@@ -53,19 +53,49 @@ void Application::initWindow() {
 		std::cerr << red << "failed to initialise GLAD\n" << def;// if GLAD cant start then throw an error
 	}
 
+	glViewport(0, 0, WIDTH, HEIGHT);// set the viewport size  to the size of the window
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);// tell GLFW to call this function when the viewport gets resized
+
 	std::cout << green << "project initialised!\n" << def;
 }
 
 // this is the function that contains the loop that cycles through everything the program runs on
 void Application::mainloop(){
 	std::cout << yellow << "running main loop\n" << def;
+
+	// while the application isent closed, do everything.
+	while (!glfwWindowShouldClose(window)){
+		// take input from the user
+		processInput();
+
+		// render the aggressively magenta window
+		glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// check all call events and swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 }
 
 // this function runs when the program is closing and it clears everything up for memory safety
 void Application::cleanup(){
 	std::cout << green << "cleaning up!\n" << def;
 
+	glfwDestroyWindow(window);// destroy the window class
 	glfwTerminate();// this closes off GLFW
 
 	std::cout << green << "cleanup finished!\n" << def;
+}
+
+// this function changes the size of the OpenGL viewport when the window resizes
+void Application::framebuffer_size_callback(GLFWwindow* window, int width, int height){
+	glViewport(0,0, width, height);
+}
+
+// get some input data from GLFW to close the window when esc is pressed
+void Application::processInput(){
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
--- src/include/app.hxx
+++ src/include/app.hxx
@@ -14,4 +14,8 @@ private:
 	void initWindow();
 	void mainloop();
 	void cleanup();
+
+	static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+	void processInput();
 };
