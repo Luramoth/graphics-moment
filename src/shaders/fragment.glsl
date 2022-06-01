150d8be619d412957fc9aef759524eb9903bbcbd
LuraMoth <fasreaperearnest@gmail.com>
2022-05-31 21:59:41 -0700

    rename variable

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -4,9 +4,9 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform sampler2D ourTexture;
+uniform sampler2D tex;
 
 void main()
 {
-    FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);  
+    FragColor = texture(tex, TexCoord) * vec4(ourColor, 1.0);  
 }
