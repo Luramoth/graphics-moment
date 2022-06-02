b67889b3c8a95e0c0ce37bc20ec8ebada2ff1d00
LuraMoth <fasreaperearnest@gmail.com>
2022-06-01 23:52:49 -0700

    attempt some transformations

--- src/opgl.cxx
+++ src/opgl.cxx
@@ -2,9 +2,12 @@
 #include "include/colormod.hxx"
 #include "include/shader.hxx"
 
+//glm stuff
+#include "include/glm/glm.hpp"
+#include "include/glm/gtc/matrix_transform.hpp"
+#include "include/glm/gtc/type_ptr.hpp"
 
 #include <GLFW/glfw3.h>
-#include <cstddef>
 #include <iostream>
 
 
@@ -56,6 +59,13 @@ OpenGL::OpenGL(GLFWwindow* window){
 	///texture shit///
 	tex.Bind(&baseShaders, "texture1", 0);
 	tex2.Bind(&baseShaders, "texture2", 1);
+
+	///transform shit///
+	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	glm::mat4 trans = glm::mat4(1.0f);
+	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	vec = trans * vec;
+	std::cout << vec.x << vec.y << vec.z << std::endl;
 }
 
 void OpenGL::GLRender(){
