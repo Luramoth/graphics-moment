8e173d36671c6f1620c92b3a704dfe0b41c2e66c
LuraMoth <fasreaperearnest@gmail.com>
2022-06-05 10:59:32 -0700

    moar cubes

--- src/opgl.cxx
+++ src/opgl.cxx
@@ -175,7 +175,7 @@ void OpenGL::GLRender(){
 		model = glm::translate(model, cubePositions[i]);
 		float angle = 20.0f * i; 
 		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-		baseShaders.setFloat("model", model);
+		baseShaders.setMat4("model", model);
 
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 	}
